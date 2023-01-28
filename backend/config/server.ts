ec7a78e8742c469beb34b47354967b4d73383061
starzje <istarcevic16@gmail.com>
2023-01-28 22:40:07 +0100

    database fix

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("strapi-production-c7c6.up.railway.app", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
