2d5de9d08acb1c70bf33dc065b340f9d629643c8
starzje <istarcevic16@gmail.com>
2023-01-29 17:28:43 +0100

    deployment fix

--- backend/config/env/production/middlewares.js
+++ backend/config/env/production/middlewares.js
@@ -23,16 +23,3 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
-
-// module.exports = ({ env }) => ({
-//   upload: {
-//     config: {
-//       provider: "cloudinary",
-//       providerOptions: {
-//         cloud_name: env("CLOUDINARY_NAME"),
-//         api_key: env("CLOUDINARY_KEY"),
-//         api_secret: env("CLOUDINARY_SECRET"),
-//       },
-//     },
-//   },
-// });
