4ad20024c612f5bb625105660f6e4d999ab5b8df
starzje <istarcevic16@gmail.com>
2023-01-29 16:41:39 +0100

    cloudinary plugin middleware

--- backend/config/env/production/middlewares.js
+++ backend/config/env/production/middlewares.js
@@ -23,3 +23,16 @@ module.exports = [
   "strapi::favicon",
   "strapi::public",
 ];
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+    },
+  },
+});
