fe0cba6a9733cfbc0a97b31f14667713856d751b
starzje <istarcevic16@gmail.com>
2023-01-29 17:12:48 +0100

    clouddinary hotfiix v1

--- backend/config/env/production/middlewares.js
+++ backend/config/env/production/middlewares.js
@@ -24,15 +24,15 @@ module.exports = [
   "strapi::public",
 ];
 
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "cloudinary",
+//       providerOptions: {
+//         cloud_name: env("CLOUDINARY_NAME"),
+//         api_key: env("CLOUDINARY_KEY"),
+//         api_secret: env("CLOUDINARY_SECRET"),
+//       },
+//     },
+//   },
+// });
--- backend/config/env/production/plugins.js
+++ backend/config/env/production/plugins.js
@@ -7,6 +7,23 @@ module.exports = ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: "cloudinary",
+//       providerOptions: {
+//         cloud_name: env("CLOUDINARY_NAME"),
+//         api_key: env("CLOUDINARY_KEY"),
+//         api_secret: env("CLOUDINARY_SECRET"),
+//       },
+//     },
+//   },
+// });
